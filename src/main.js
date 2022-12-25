4a6d1ce4a67e9b816e263c63b0d1fa2994d6edd4
fangliji <fangliji@shencom.cn>
2022-12-25 21:27:42 +0800

    remove vuex

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import { createApp } from 'vue';
 import './style.css';
 import router from './router/index';
-import store from './store/index';
 import App from './App.vue';
 
-createApp(App).use(router).use(store).mount('#app');
+createApp(App).use(router).mount('#app');
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,22 +1,8 @@
 <script setup>
-import { useStore } from 'vuex';
 import { provide, computed } from 'vue';
-import injectcom from '../components/injectcom.vue';
-const store = useStore();
-provide('name', {
-  data: 3,
-  data2: 33,
-});
-console.log(store.state.count, 33);
-const count = computed(() => store.state.count);
-function asyncAdd() {
-  store.dispatch('asyncAdd');
-}
 </script>
 <template>
-  <div @click="asyncAdd">home</div>
-  <div>{{ count }}</div>
-  <injectcom />
+  <div>home</div>
 </template>
 
 <style lang="scss" scoped></style>
