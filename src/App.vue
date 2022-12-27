f147f270f2ffc8147f0836e4c63efd5c5ec5c420
fangliji <fangliji@shencom.cn>
2022-12-27 20:24:49 +0800

    新增element3

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,38 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue';
+import Header from './components/header.vue';
 </script>
 
 <template>
-  <div>
-    <router-link to="/">首页</router-link>|
-    <router-link to="/about">about</router-link>
-    <!-- <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a> -->
-  </div>
-  <router-view></router-view>
-  <!-- <HelloWorld msg="Vite + Vue" /> -->
+  <el-container>
+    <Header />
+    <el-container>
+      <el-aside width="200px">
+        <div><router-link to="/"> Home</router-link></div>
+        <div><router-link to="/about">About</router-link></div>
+      </el-aside>
+      <el-container>
+        <el-main> <router-view></router-view> </el-main>
+      </el-container>
+    </el-container>
+  </el-container>
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
+.el-header,
+.el-footer {
+  background-color: #b3c0d1;
+  color: #333;
+  text-align: center;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+.el-aside {
+  background-color: #d3dce6;
+  color: #333;
 }
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+.el-main {
+  background-color: #e9eef3;
+  color: #333;
+}
+body > .el-container {
+  margin-bottom: 40px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import { createApp } from 'vue';
 import './style.css';
 import router from './router/index';
+import Element3 from 'element3';
+import 'element3/lib/theme-chalk/index.css';
 import { createPinia } from 'pinia';
 import App from './App.vue';
 
-createApp(App).use(router).use(createPinia()).mount('#app');
+createApp(App).use(router).use(createPinia()).use(Element3).mount('#app');
